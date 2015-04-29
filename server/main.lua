e195e3aa80cab9e051f9ba6b8e0500f9c501199c
KozzX <andre@andrecandido.com>
2015-04-29 09:48:39 -0300

    Commit

--- server/main.lua
+++ server/main.lua
@@ -3,67 +3,18 @@
 --======================================================================--
 local gs = require( 'CoroniumGS' ):new( 7173, 'abc' )
 
+
 _G.gs = gs
 --======================================================================--
 --== Game Code
 --======================================================================--
 --local ClientData = require( 'ClientData' ).new()
+local Barras = require( 'Barras' )
+local bar = Barras:new()
 local DataProcessor = require( "DataProcessor" )
 local dp = DataProcessor:new()
 
 --== Game Code Goes Here
-
-local function calculateHit(client,hit)
-	local game = gs:getPlayerGame (client)
-	local win = 0
-
-	local player_num = client:getPlayerNum()
-	p(player_num)
-
-	player1 = game:getPlayerByNumber(1)
-	player2 = game:getPlayerByNumber(2)
-
-	if player_num == 1 then
-		game.bar2 = game.bar2 - hit
-		game.bar1 = game.bar1 + hit
-
-	else
-		game.bar1 = game.bar1 - hit
-		game.bar2 = game.bar2 + hit
-	end
-
-	if game.bar1 == 100 then
-		--game:publishGameDone("bye")
-		--game:close( )
-		win = 1
-	elseif game.bar2 == 100 then
-		--game:publishGameDone("bye")
-		--game:close( )
-		win = 2
-	end
-	p("Winner:",win)
-	player1:send( { setbar = { bar1 = game.bar1, bar2= game.bar2, win=win } } )
-	player2:send( { setbar = { bar1 = game.bar2, bar2= game.bar1, win=win } } )
-
-end
-
-local function restart( client )
-	local game = gs:getPlayerGame (client)
-	local player_num = client:getPlayerNum()
-	game.bar1 = 50
-	game.bar2 = 50
-	if player_num == 1 then
-		game.ready1 = 1
-	elseif player_num == 2 then
-		game.ready2 = 1
-	end
-	p("Restart:",game.ready1,game.ready2)
-	if game.ready1 == 1 and game.ready2 == 1 then
-		game:broadcast({ setbar = { bar1 = game.bar1, bar2 = game.bar2, restart = 1 } })
-		game.ready1 = 0
-		game.ready2 = 0
-	end
-end
 
 
 --======================================================================--
@@ -72,12 +23,8 @@ end
 local function onGameCreate( game )
 	p( "--== Game Created ==--" )
 	p( game:getId() )
-
-	game.bar1 = 50
-	game.bar2 = 50
-	game.ready1 = 0
-	game.ready2 = 0
-
+	bar:criar( game )
+	p("criar",game.bar1)
 end
 
 local function onGameJoin( game, player )
@@ -90,7 +37,7 @@ local function onGameStart( game, players )
 	p( "--== New Game Started " .. game:getId() .. " ==--" )
 	p( "Games", gs:getGameCount() )
 
-	game:broadcast({ setbar = { bar1 = game.bar1, bar2 = game.bar2 } })
+	bar:comecar(game)
 end
 
 local function onGameLeave( game, player )
@@ -110,9 +57,9 @@ local function onClientData( client, data )
 	dp:process( client,data )
 
 	if data.hit then
-		calculateHit( client, data.hit )
+		bar:setBar( client, data.hit )
 	elseif data.ready then
-		restart( client )
+		bar:restart( client )
 	end
 end
 
---
+++ server/modules/Barras.lua
@@ -0,0 +1,70 @@
+local Emitter = require( 'core' ).Emitter
+
+local Barras = Emitter:extend()
+function Barras:initialize( )
+
+end
+
+function Barras:criar( game )
+	game.bar1 = 50
+	game.bar2 = 50
+	game.ready1 = 0
+	game.ready2 = 0
+end
+
+function Barras:comecar( game )
+	game:broadcast({ setbar = { bar1 = game.bar1, bar2 = game.bar2 } })
+end
+
+function Barras:setBar( client,hit )
+	local game = gs:getPlayerGame (client)
+	local win = 0
+
+	local player_num = client:getPlayerNum()
+	p(player_num)
+
+	player1 = game:getPlayerByNumber(1)
+	player2 = game:getPlayerByNumber(2)
+
+	if player_num == 1 then
+		game.bar2 = game.bar2 - hit
+		game.bar1 = game.bar1 + hit
+	else
+		game.bar1 = game.bar1 - hit
+		game.bar2 = game.bar2 + hit
+	end
+
+	if game.bar1 == 100 then
+		--game:publishGameDone("bye")
+		--game:close( )
+		win = 1
+	elseif game.bar2 == 100 then
+		--game:publishGameDone("bye")
+		--game:close( )
+		win = 2
+	end
+	p("Winner:",win)
+	player1:send( { setbar = { bar1 = game.bar1, bar2= game.bar2, win=win } } )
+	player2:send( { setbar = { bar1 = game.bar2, bar2= game.bar1, win=win } } )
+			
+end
+
+function Barras:restart( client )
+	local game = gs:getPlayerGame (client)
+	local player_num = client:getPlayerNum()
+	game.bar1 = 50
+	game.bar2 = 50
+	if player_num == 1 then
+		game.ready1 = 1
+	elseif player_num == 2 then
+		game.ready2 = 1
+	end
+	p("Restart:",game.ready1,game.ready2)
+	if game.ready1 == 1 and game.ready2 == 1 then
+		game:broadcast({ setbar = { bar1 = game.bar1, bar2 = game.bar2, restart = 1 } })
+		game.ready1 = 0
+		game.ready2 = 0
+	end
+end
+
+return Barras
